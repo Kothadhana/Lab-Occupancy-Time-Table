45f86afd0ff8e798ad13e472f88bb5cfac9c9f6f
Dhana <dhanalakshmi5k9@gmail.com>
2021-05-18 00:35:43 +0530

    Initial commit

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web:gunicorn TimeTable.wsgi
+web: gunicorn TimeTable.wsgi --log-file -
--- TimeTable/settings.py
+++ TimeTable/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-pwm20hj3w5&49q9l10tidurr7sude%pj_9&7k2#a&3k^)+k41v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['infinite-basin-91656.herokuapp.com']
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -124,9 +125,17 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
 STATIC_URL = '/static/'
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+
+# Extra lookup directories for collectstatic to find static files
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+#  Add configuration for static files storage using whitenoise
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 LOGIN_URL = "lg"
@@ -136,3 +145,6 @@ LOGIN_REDIRECT_URL = "hme"
 # Configure Django App for Heroku.
 import django_heroku
 django_heroku.settings(locals())
+import dj_database_url
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
