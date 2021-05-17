dcdb5256154f748bfdb55eae427769fbc23eb269
Dhana <dhanalakshmi5k9@gmail.com>
2021-05-18 01:21:20 +0530

    Initial commit

--- TimeTable/settings.py
+++ TimeTable/settings.py
@@ -93,7 +93,16 @@ WSGI_APPLICATION = 'TimeTable.wsgi.application'
         'PORT': ''
     }
 }'''
-DATABASES = {}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': 'root',
+        'PASSSWORD': 'system',
+        'PORT': ''
+    }
+}
 DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
